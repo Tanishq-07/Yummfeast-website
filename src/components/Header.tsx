a59acd9344ec843e537abc45b1e49adda31cdb27
anuj-af <anujkhandelwal.0404@gmail.com>
2025-11-22 22:47:00 +0530

    Update Linkedin Link On Mobile View

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -93,7 +93,11 @@ export default function Header({ navigateTo, currentPage, isMenuOpen, setIsMenuO
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                   </svg>
                 </Link>
-                <Link href="https://twitter.com/yummfeast" external className="text-blue-400 hover:text-blue-500">
+                <Link
+                  href="https://linkedin.com/company/yummfeast"
+                  external
+                  className="text-blue-400 hover:text-blue-500"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -104,9 +108,11 @@ export default function Header({ navigateTo, currentPage, isMenuOpen, setIsMenuO
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className="lucide lucide-twitter"
+                    className="lucide lucide-linkedin"
                   >
-                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-14h4v2.1" />
+                    <rect width="4" height="14" x="2" y="10" />
+                    <circle cx="4" cy="4" r="2" />
                   </svg>
                 </Link>
               </div>
